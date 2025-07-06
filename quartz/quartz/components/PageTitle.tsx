3b49cee9a2546706a1c862b846dbeb56ed48474b
Alexey Chernyavskiy <pprometey@gmail.com>
2025-07-06 13:40:04 +0500

    customize quartz

--- quartz/quartz/components/PageTitle.tsx
+++ quartz/quartz/components/PageTitle.tsx
@@ -8,7 +8,7 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const baseDir = pathToRoot(fileData.slug!)
   return (
     <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
+      <a href={baseDir}><span class="logo"></span><span class="title-text">{title}</span></a>
     </h2>
   )
 }
